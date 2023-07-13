3f2c98c3ef9d43d7f72de8df37c9ad5092861bd2
taewook noh <briannoh@tnear.com>
2023-07-13 16:33:50 +0900

    Initial Commit

---
+++ FigmaTokenTest/FigmaTokenTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaTokenTestApp.swift
+//  FigmaTokenTest
+//
+//  Created by Taewook Noh on 2023/07/13.
+//
+
+import SwiftUI
+
+@main
+struct FigmaTokenTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
